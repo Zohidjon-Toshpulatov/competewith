db6bf83cf68008123384412e91d3efb24c8404b2
Zohidjon-Toshpulatov <44026027+Zohidjon-Toshpulatov@users.noreply.github.com>
2022-08-08 14:54:19 +0500

    Projec crated

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary": '#725095',
+        "secondary": "#fdc010",
+        "cus-black": '#30273A'
+      },
+      backgroundImage: {
+        'cards': "url('./assets/bg-primary.png')",
+      }
+    },
+  },
+  plugins: [],
+}
