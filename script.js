3bf572b05d31c1c2a172a624cab8b00bdb4ca465
Zohidjon-Toshpulatov <44026027+Zohidjon-Toshpulatov@users.noreply.github.com>
2022-08-09 22:19:03 +0500

    mobile menu

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const toggleBtn = document.querySelectorAll('.btn-toggle')[0];
 const mobileNav = document.getElementById('mobile-nav');
 
-addEventListener('click', () => {
+toggleBtn.addEventListener('click', () => {
     const cList = new Array(...mobileNav.classList);
     if (cList.includes('hidden')) {
         mobileNav.classList.replace('hidden', 'flex')
