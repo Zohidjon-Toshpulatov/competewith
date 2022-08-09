1e073dae9cc773f76bfcf9c79d8298a11095ca37
Zohidjon-Toshpulatov <44026027+Zohidjon-Toshpulatov@users.noreply.github.com>
2022-08-09 15:15:00 +0500

    It is ready to test

--- script.js
+++ script.js
@@ -8,4 +8,32 @@ addEventListener('click', () => {
     } else if (cList.includes('flex')) {
         mobileNav.classList.replace('flex', 'hidden')
     }
-})
+});
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        "@0.00": {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        "@0.75": {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        "@1.00": {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        "@1.50": {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+    },
+});
